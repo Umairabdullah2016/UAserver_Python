b79a413a10db1421581bf9c2d52230668da4b7ff
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-07 19:25:37 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+# Display custom HTML content
+htmlf = '''<iframe width="392" height="620" style="border: 0px;" src="https://studio.code.org/projects/applab/3w-53qDYJIXBqt3PX30J9RaZ79aE7aecDq1Mk-4szJg/embed"></iframe>'''
+st.html(
+   htmlf,
+   width=600
+)
