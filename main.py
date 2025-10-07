a8b9c9e4552fbd540e1bfc88c9a4cd5b898dacf5
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-07 20:49:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Display custom HTML content
-htmlf = '''<iframe width="392" height="620" style="border: 0px;" src="https://studio.code.org/projects/applab/3w-53qDYJIXBqt3PX30J9RaZ79aE7aecDq1Mk-4szJg/embed"></iframe>'''
+st.title("UAserver Mobile Emulator")
 
-components.html(
-    htmlf,
-    height=650,  # Adjust height if needed
-    width=600
-)
+
+htmlf = '''<iframe width="392" height="620" style="border: 0px;" 
+src="https://studio.code.org/projects/applab/3w-53qDYJIXBqt3PX30J9RaZ79aE7aecDq1Mk-4szJg/embed"></iframe>'''
+
+components.html(htmlf, height=650, width=600)
