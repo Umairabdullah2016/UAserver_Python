20c15bdc99abe6f1ce618a0fd740d24a06fb059f
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-07 20:44:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
 # Display custom HTML content
 htmlf = '''<iframe width="392" height="620" style="border: 0px;" src="https://studio.code.org/projects/applab/3w-53qDYJIXBqt3PX30J9RaZ79aE7aecDq1Mk-4szJg/embed"></iframe>'''
-st.html(
-   htmlf,
-   width=600
+
+components.html(
+    htmlf,
+    height=650,  # Adjust height if needed
+    width=600
 )
