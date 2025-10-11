5b3b347a3fa32924706e1c1bbbd746ad5f192d43
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-11 17:30:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='python5',
+    version='0.1.0',
+    packages=['python5'],  # 👈 Explicitly list the package
+    install_requires=[],   # Optional: list dependencies here
+    author='Umair Abdullah',
+    description='My python5 package from GitHub',
+)
