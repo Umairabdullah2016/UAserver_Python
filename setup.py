2ebbadca1341c99734527fb6b65e536a9a5035dc
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-12 10:13:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='python5',
     version='0.1.0',
-    packages=['python5',
-             "ZuhairGaming"],  # 👈 Explicitly list the package
-    install_requires=[],   # Optional: list dependencies here
+    packages=['python5', "ZuhairGaming"],  # 📝 Explicitly list the package
+    install_requires=[],    # Optional: list dependencies here
     author='Umair Abdullah',
-    description='My python5 package from GitHub',
+    description='My Python Package',
 )
