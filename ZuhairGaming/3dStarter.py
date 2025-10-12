ddd8714abd879a6c9a66308a7bdeeb2c488ad01d
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-12 10:06:29 +0100

    Create 3dStarter.py

---
+++ ZuhairGaming/3dStarter.py
@@ -0,0 +1,21 @@
+from ursina import *
+from ursina.prefabs.firstpersoncontroller import FirstPersonCortroller
+def start_game():
+  player = FirstPersonController()
+  ground = Entity(
+  model = "plane",
+  collider = "box",
+  texture = color.green,
+  scale = (100 , 100 , 100)
+  )
+  direction = (hit_pos - camera.world_position).normalized()
+  hit_pos = mouse.world_point +  direction * 2
+  if key == "left mouse down":
+    cube = Entity(
+      model = "box",
+      collider = "box",
+      scale = (2,2,2),
+      postition = hit_pos
+    )
+    
+  
