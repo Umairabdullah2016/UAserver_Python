f1bfaff2aaea63cda89fec9daa82d8c18426bfd1
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-12 11:14:50 +0100

    Update main.py

--- emulator_dir/main.py
+++ emulator_dir/main.py
@@ -1,4 +1,9 @@
-import streamlit
+import streamlit as st
+import streamlit.components.v1 as components
+import subprocess
+import site
+import webbrowser
+
 def start_uaserver():
     st.title("UAserver Mobile Emulator")
 
@@ -6,13 +11,9 @@ def start_uaserver():
     htmlf = '''<iframe width="392" height="620" style="border: 0px;" 
     src="https://studio.code.org/projects/applab/3w-53qDYJIXBqt3PX30J9RaZ79aE7aecDq1Mk-4szJg/embed"></iframe>'''
     path = site.getusersitepackages()
-    stp = path,"/python5/main.py"
-    cmd = f"streamlit run {stp} --server.port 9000"
+    stp = f"{path}/python5/main.py"
+    cmd = f"python -m streamlit run {stp} --server.port 9000"
     components.html(htmlf, height=650, width=600)
-    result = subprocess.run(
-        ["powershell", cmd , "Get-Process"],
-        capture_output=True,
-        text=True
-        )
+    cmd = ["streamlit", "run", stp, "--server.port", "9000"]
     webbrowser.open("tinyurl.com/localhostprovider")
     
