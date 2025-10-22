947dda9918e0d98f4ca83229beb7561e614d42fb
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-22 13:57:59 +0100

    Update package list in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='UAserver_Python',
     version='0.1.0',
-    packages=['python5', "ZuhairGaming", "emulator_dir"],  # 📝 Explicitly list the package
+    packages=["BaldiWindows", "ZuhairGaming", "emulator", "python5", "tkinterHP"],  # 📝 Explicitly list the package
     install_requires=[],    # Optional: list dependencies here
     author='Umair Abdullah',
     description='My Python Package',
