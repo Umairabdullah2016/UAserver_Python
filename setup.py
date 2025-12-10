e46caf81cf31e3597af7733a3d6ebb56dfe7e970
Umairabdullah2016 <ua8839705@gmail.com>
2025-12-10 16:40:24 +0000

    Update Python version compatibility in setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
     install_requires=[],             # Add dependencies here if needed
     author='Umair Abdullah',         # Your name
     description='My Python Package', # Short description
-    python_requires='>=3.13, <3.15', # Compatible with Python 3.13 and 3.14
+    python_requires='>=3.10, <3.15', # Compatible with Python 3.13 and 3.14
     include_package_data=True,       # Include non-Python files if specified in MANIFEST.in
 )
